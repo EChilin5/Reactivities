b92ebedd6c2293d82bb19cf0f2301a6143c5b585
EChilin5 <edgar3ac@gmail.com>
2022-02-08 14:49:41 -0800

    end of section 2

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    // controller placeholder replaced by the speciifed controller class
+    [Route("api/[controller]")] // api/activites
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+}
